65697a073c050de0a27d216adb926c81427f29b7
Julien Sanchez <sumbobyboys@gmail.com>
2020-04-05 02:48:18 +0200

    Updates

---
+++ precache-manifest.9555e586225465ea01caf8c6971bdc4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "005cc7cc2d9c4ffcc06c6bb7d36247c1",
+    "url": "/opz-gif/index.html"
+  },
+  {
+    "revision": "5c554c63e14755ebdea1",
+    "url": "/opz-gif/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "9fe38dd6dc1aa80834ad",
+    "url": "/opz-gif/static/js/2.863e4aa7.chunk.js"
+  },
+  {
+    "revision": "0b08544f54c9906705be39c03b7e79ad",
+    "url": "/opz-gif/static/js/2.863e4aa7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c554c63e14755ebdea1",
+    "url": "/opz-gif/static/js/main.6a040b5c.chunk.js"
+  },
+  {
+    "revision": "45cf7a606e62195fa8a6",
+    "url": "/opz-gif/static/js/runtime-main.634d7a3d.js"
+  }
+]);
