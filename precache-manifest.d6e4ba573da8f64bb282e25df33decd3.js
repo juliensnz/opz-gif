c32bb490ec0df529307e966e5944db95d3339d7b
Julien Sanchez <sumbobyboys@gmail.com>
2020-04-27 20:58:02 +0200

    Updates

--- precache-manifest.d6e4ba573da8f64bb282e25df33decd3.js
+++ precache-manifest.d6e4ba573da8f64bb282e25df33decd3.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "9217ce53b369138e2cab700bf1e7b52a",
+    "revision": "d189758bf7b69d3071cd5c972541276d",
     "url": "/opz-gif/index.html"
   },
   {
-    "revision": "6381c932aa6addfa7e42",
+    "revision": "d98bd62bbfeeda3103d0",
     "url": "/opz-gif/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "e04c1d68b0987980ae4d",
-    "url": "/opz-gif/static/js/2.f057d089.chunk.js"
+    "revision": "d1fedad7a251fb997442",
+    "url": "/opz-gif/static/js/2.cbabf47d.chunk.js"
   },
   {
     "revision": "ae595edd83e4d4c693a705cbba5a7a41",
-    "url": "/opz-gif/static/js/2.f057d089.chunk.js.LICENSE.txt"
+    "url": "/opz-gif/static/js/2.cbabf47d.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "6381c932aa6addfa7e42",
-    "url": "/opz-gif/static/js/main.edba0b27.chunk.js"
+    "revision": "d98bd62bbfeeda3103d0",
+    "url": "/opz-gif/static/js/main.262a16ac.chunk.js"
   },
   {
     "revision": "45cf7a606e62195fa8a6",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/opz-gif/precache-manifest.974fc8c56601b2d0b47504ad07930801.js"
+  "/opz-gif/precache-manifest.d6e4ba573da8f64bb282e25df33decd3.js"
 );
 
 self.addEventListener('message', (event) => {
